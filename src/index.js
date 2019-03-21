4cd65260c02efe094abfffbc781dd83bf26cc2ee
Julio <julio.gonzalez2014a@gmail.com>
2019-03-20 19:38:38 -0500

    version 2

--- src/index.js
+++ src/index.js
@@ -2,4 +2,13 @@ const express = require('express');
 
 const config = require('./server/config');
 
-config(express());
+// database
+require('./database');
+
+const app = config(express());
+
+// starting the server
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
+
+});
